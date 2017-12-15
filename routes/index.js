6d3609e999affba81c7cc1211ce1d3d851a61e49
Philip <philip_klingzell@hotmail.com>
2017-12-15 13:22:54 +0100

    Added chat

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
     res.render('index', { title: 'Express' });
 });
 
+router.get('/chat', function(req, res) {
+    res.render('chat', { title: 'Chatt' });
+});
+
+
 module.exports = router;
