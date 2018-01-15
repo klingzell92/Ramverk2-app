1f3fd2134515014b87832abb881c2691228d3ea6
Philip Klingzell <philip_klingzell@hotmail.com>
2018-01-15 19:41:40 +0100

    Tests for connecting to websocket

--- test/tictactoe/tictactoeTest.js
+++ test/tictactoe/tictactoeTest.js
@@ -6,22 +6,19 @@ const after = mocha.after;
 const WebSocket = require("ws");
 const it = mocha.it;
 const tictactoeServer = require('../../src/tictactoe/tictactoe.js');
-let websocket;
 let url = "ws://localhost:1338/";
 
-describe("Suite of unit tests for nim-server", function() {
+describe("Testing websocket", function() {
     var websocket;
-
     before(async function() {
-        console.log("Starting test-server.");
         await tictactoeServer.listen();
     });
     after(async function() {
         await tictactoeServer.close(function() {
-            console.log('Closing test-server.');
+            console.log('Closing the server');
         });
     });
-    describe('Test Web Socket.', function () {
+    describe('Test connecting player1.', function () {
         it('Add player1', (done) => {
             websocket = new WebSocket(url);
             websocket.onopen = () => {
@@ -42,28 +39,29 @@ describe("Suite of unit tests for nim-server", function() {
             done();
         });
     });
-    describe('Test Web Socket.', function () {
-        it('Add player2', (done) => {
-            websocket2 = new WebSocket(url);
-            websocket2.onopen = function() {
-                console.log("The websocket is now open.");
-                let object = {
-                    type: "connect",
-                    player: "player2"
-                };
-
-                websocket2.send(JSON.stringify(object));
-            };
-            websocket2.onmessage = function(event) {
-                let data = JSON.parse(event.data);
-                if (data.size === 2) {
-                    assert.equal(data.size, 2)
-                }
-            };
-            done();
-        });
-    });
 });
 /*
+
+describe('Test connecting player2.', function () {
+    it('Add player2', (done) => {
+        websocket2 = new WebSocket(url);
+        websocket2.onopen = function() {
+            console.log("The websocket is now open.");
+            let object = {
+                type: "connect",
+                player: "player2"
+            };
+
+            websocket2.send(JSON.stringify(object));
+        };
+        websocket2.onmessage = function(event) {
+            let data = JSON.parse(event.data);
+            if (data.size === 2) {
+                assert.equal(data.size, 2)
+            }
+        };
+        done();
+    });
+});
 */
 //tictactoeServer.close();
