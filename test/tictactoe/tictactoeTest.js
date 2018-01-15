b7d73ec18b88ab6e6d7bcd659b43af7a15fdf69a
Philip Klingzell <philip_klingzell@hotmail.com>
2018-01-15 18:23:04 +0100

    Removed before and after

--- test/tictactoe/tictactoeTest.js
+++ test/tictactoe/tictactoeTest.js
@@ -8,34 +8,24 @@ const it = mocha.it;
 const tictactoeServer = require('../../src/tictactoe/tictactoe.js');
 
 describe('Test Web Socket.', function () {
-    before(async function() {
-        console.log("Opening server");
-        await tictactoeServer.listen();
+    it('Add players', (done) => {
+        let ws = new WebSocket("ws://127.0.0.1:1338");
+        ws.onopen = function() {
+            let player1 = {
+                type: "connect",
+                player: "player1"
+            };
+            ws.send(JSON.stringify(player1));
+        };
+        ws.onmessage = function(event) {
+            let data = JSON.parse(event.data);
+            console.log(data);
+            if (data.size === 1) {
+                assert.equal(data.size, 1)
+            }
+        };
+        done();
     });
 
-    after(async function() {
-        await tictactoeServer.close(function() {
-            console.log('Closing test-server.');
-        });
-    });
-    describe('Connect players', () => {
-        it('Add players', (done) => {
-            let ws = new WebSocket("ws://127.0.0.1:1338");
-            ws.onopen = function() {
-                let player1 = {
-                    type: "connect",
-                    player: "player1"
-                };
-                ws.send(JSON.stringify(player1));
-            };
-            ws.onmessage = function(event) {
-                let data = JSON.parse(event.data);
-                console.log(data);
-                if (data.size === 1) {
-                    assert.equal(data.size, 1)
-                }
-            };
-            done();
-        });
-    });
 });
+tictactoeServer.close();
